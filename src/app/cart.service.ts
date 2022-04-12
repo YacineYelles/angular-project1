0bcdef17b8324216cd05eb3fe8de6737819167b0
YacineYelles <yaciney868@gmail.com>
2022-04-12 10:10:03 +0200

    2 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -22,7 +22,9 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 
   constructor(private http: HttpClient) {}
